4583de8a33ad7678e13986c2032d4c4a1cf91073
=efren <=a23efrencv@iessanclemente.net>
2024-06-03 12:09:42 +0200

    probas lunes

--- src/test/java/dev/selenium/ChromeTest.java
+++ src/test/java/dev/selenium/ChromeTest.java
@@ -1,17 +1,44 @@
 package dev.selenium;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class ChromeTest{
+public class ChromeTest {
 
- ChromeDriver driver;
+   ChromeDriver driver;
 
- @Test  
- public void test(){
-    ChromeOptions options = new ChromeOptions();
-    driver = new ChromeDriver(options);
- }
+   @BeforeAll
+   public void before() {
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("start-maximized");
+      driver = new ChromeDriver(options);
 
+      driver.get("file:///home/sanclemente.local/a23efrencv/Escritorio/CD_UD3_Selenium_maven/src/index.html");
+      try {
+         Thread.sleep(5000);
+      } catch (InterruptedException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+   }
+
+   @Test
+   public void test1() {
+      WebElement inputUser = driver.findElement(By.id("inputUser"));
+      WebElement inputPass = driver.findElement(By.id("inputPass"));
+      WebElement searchButton = driver.findElement(By.className("btn btn-primary"));
+
+   }
+
+   @AfterAll
+   public void after() {
+      driver.quit();
+
+   }
 }
