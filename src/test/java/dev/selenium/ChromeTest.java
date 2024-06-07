bc22279d019a22f1917d8b52e2c88a98e0c31f5b
=efren <=a23efrencv@iessanclemente.net>
2024-06-07 12:05:30 +0200

    novas probas

--- src/test/java/dev/selenium/ChromeTest.java
+++ src/test/java/dev/selenium/ChromeTest.java
@@ -1,5 +1,7 @@
 package dev.selenium;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,20 +9,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v118.v118CdpInfo;
 
 public class ChromeTest {
 
-   ChromeDriver driver;
+   static ChromeDriver driver;
 
    @BeforeAll
-   public void before() {
+   public static void before() {
       ChromeOptions options = new ChromeOptions();
       options.addArguments("start-maximized");
       driver = new ChromeDriver(options);
 
       driver.get("file:///home/sanclemente.local/a23efrencv/Escritorio/CD_UD3_Selenium_maven/src/index.html");
       try {
-         Thread.sleep(5000);
+         Thread.sleep(2000);
       } catch (InterruptedException e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
@@ -30,14 +33,50 @@ public class ChromeTest {
 
    @Test
    public void test1() {
+
+      // Buscamos los elementos.
       WebElement inputUser = driver.findElement(By.id("inputUser"));
       WebElement inputPass = driver.findElement(By.id("inputPass"));
-      WebElement searchButton = driver.findElement(By.className("btn btn-primary"));
+      WebElement searchButton = driver.findElement(By.className("btn-primary"));
+
+      inputUser.sendKeys("efren");
+      inputPass.sendKeys("abc123.");
+      // Duerme 2 seg(Theread siempre va dentro de un try/catch)
+      try {
+         Thread.sleep(2000);
+      } catch (InterruptedException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      searchButton.click();
+
+      // assertEquals(h1 , "Formulario procesado");
+      // Primero buscamos el elemento(tiene que ser de forma secuencial).
+      WebElement h1 = driver.findElement(By.tagName("h1"));
+
+      // Segundo comparamos el elemento con el texto de dicho elemento.
+      assertEquals("Formulario procesado", h1.getText());
+   }
+
+   @Test
+   public void test2() {
+      WebElement inputCheck = driver.findElement(By.className("form-check-input"));
+      WebElement searchButton = driver.findElement(By.className("btn-primary"));
+
+      try {
+         Thread.sleep(2000);
+      } catch (InterruptedException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+      inputCheck.click();
+      searchButton.click();
 
    }
 
    @AfterAll
-   public void after() {
+   public static void after() {
       driver.quit();
 
    }
