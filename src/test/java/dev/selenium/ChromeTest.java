9e19ae087251c489863ac139509660220d3c45c5
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2024-06-16 19:44:37 +0200

    probas

--- src/test/java/dev/selenium/ChromeTest.java
+++ src/test/java/dev/selenium/ChromeTest.java
@@ -21,7 +21,7 @@ public class ChromeTest {
       options.addArguments("start-maximized");
       driver = new ChromeDriver(options);
 
-      driver.get("file:///home/sanclemente.local/a23efrencv/Escritorio/CD_UD3_Selenium_maven/src/index.html");
+      driver.get("file:/Users/efrencorzonvazquez/Desktop/CD_UD3_Selenium_maven/src/index.html");
       try {
          Thread.sleep(2000);
       } catch (InterruptedException e) {
@@ -37,18 +37,21 @@ public class ChromeTest {
       // Buscamos los elementos.
       WebElement inputUser = driver.findElement(By.id("inputUser"));
       WebElement inputPass = driver.findElement(By.id("inputPass"));
-      WebElement searchButton = driver.findElement(By.className("btn-primary"));
 
-      inputUser.sendKeys("efren");
+      // Escribimos formulario
+      inputUser.sendKeys("Efren");
       inputPass.sendKeys("abc123.");
       // Duerme 2 seg(Theread siempre va dentro de un try/catch)
       try {
-         Thread.sleep(2000);
+         Thread.sleep(3000);
       } catch (InterruptedException e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
-      searchButton.click();
+
+      // Enviar botón.
+      WebElement btnSearch = driver.findElement(By.className("btn-primary"));
+      btnSearch.click();
 
       // assertEquals(h1 , "Formulario procesado");
       // Primero buscamos el elemento(tiene que ser de forma secuencial).
@@ -56,23 +59,24 @@ public class ChromeTest {
 
       // Segundo comparamos el elemento con el texto de dicho elemento.
       assertEquals("Formulario procesado", h1.getText());
+
    }
 
+   // Hacer click en "Check me out" y hacer click en "Confirm".
    @Test
    public void test2() {
       WebElement inputCheck = driver.findElement(By.className("form-check-input"));
-      WebElement searchButton = driver.findElement(By.className("btn-primary"));
+      WebElement btn = driver.findElement(By.className("btn-primary"));
 
       try {
-         Thread.sleep(2000);
+         Thread.sleep(3000);
       } catch (InterruptedException e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
 
       inputCheck.click();
-      searchButton.click();
-
+      btn.click();
    }
 
    @AfterAll
